f3300d339dd1e4dda7ce04049a6a0430beb48909
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-09-03 17:51:47 -0300

    configure the ServiceProvider for registering services, and our own Dependency Resolver

--- DIAmbientContextThreadLocalStorage/DIAmbientContextThreadLocalStoragePresentation/Global.asax.cs
+++ DIAmbientContextThreadLocalStorage/DIAmbientContextThreadLocalStoragePresentation/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DIAmbientContextThreadLocalStoragePresentation.App_Start;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,12 +12,20 @@ namespace DIAmbientContextThreadLocalStoragePresentation
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+
+        public static IServiceProvider ServiceProvider { get; private set; }
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var services = new ServiceCollection();
+
+            ServiceProvider = services.BuildServiceProvider();
+
+            DependencyResolver.SetResolver(new ServiceProviderDependencyResolver(ServiceProvider));
         }
     }
 }
